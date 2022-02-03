4d756dc3ccb0d77ad63b8e1c4b80c5a435770442
HYLZ-2019 <1657181739@qq.com>
2022-02-03 19:57:38 +0800

    Coded some framework.

---
+++ backend.js
@@ -0,0 +1,8 @@
+// Code for the backend.
+
+/** Query the pdf metadata of the current page, and get is reading progress data from the extension's inner database. */
+function loadData(){
+    // TODO
+    console.log("TODO: loadData.");
+    return 0;
+}
---
+++ mapbar.js
@@ -0,0 +1,37 @@
+// The script ran when the extension popup's button is clicked.
+
+showBar();
+
+function showBar() {
+    let sideBar = document.getElementById("ReadingMapBar");
+    if (sideBar === null){
+        // Create a new bar.
+        sideBar = document.createElement("div");
+        sideBar.setAttribute("id", "ReadingMapBar");
+        sideBar.setAttribute("class", "readingMapBar");
+        
+        // Insert the stylesheet.
+        let style = document.createElement("link");
+        style.setAttribute("rel", "stylesheet");
+        style.setAttribute("href", "mapbar.css");
+        document.body.appendChild(style);
+        
+        // Insert the bar.
+        document.body.appendChild(sideBar);
+    }
+    let data = loadData();
+    buildContents(sideBar, data);
+}
+
+
+/** Draw the contents of the side bar. */
+function buildContents(sidebar, data){
+    // TODO
+    console.log(data);
+    sidebar.innerHTML = "Sidebar In Progress.";
+}
+
+
+
+
+
