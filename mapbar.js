60074c49f57c8b84bcfd71c9268ef02c6c88f9cc
HYLZ-2019 <1657181739@qq.com>
2022-02-06 12:53:30 +0800

    Make the bar's position harmonious by reopening the original pdf in an iframe beside it.

--- mapbar.js
+++ mapbar.js
@@ -3,13 +3,27 @@
 showBar();
 
 function showBar() {
+    console.log(window.location.href)
+    let url = window.location.href;
     let sideBar = document.getElementById("ReadingMapBar");
     if (sideBar === null){
+        // Clear the entire page.
+        document.body.innerHTML = "";
+        
+        // Make a new div to hold our things.
+        let div = document.createElement("div");
+        div.setAttribute("id", "readingMapBigBox");
+        document.body.appendChild(div);
         // Create a new bar.
         sideBar = document.createElement("div");
-        sideBar.setAttribute("id", "ReadingMapBar");
-        sideBar.setAttribute("class", "readingMapBar");        
-        document.body.appendChild(sideBar);
+        sideBar.setAttribute("id", "readingMapBar");   
+        div.appendChild(sideBar);
+        // Reopen the original window in an iframe.
+        iframe = document.createElement("iframe");
+        iframe.setAttribute("src", url);
+        iframe.setAttribute("id", "readingMapIframe");
+        iframe.setAttribute("title", "The original PDF reading page.")
+        div.appendChild(iframe);
     }
     let data = loadData();
     buildContents(sideBar, data);
